0e85946cb8e29697e7658802fcbcc5c43b9bb6c2
Aaron(constant94-dev) <constant94@naver.com>
2024-01-22 22:54:04 +0900

    feat(controller): put, delete 메서드 생성

---
+++ src/main/java/com/boot/basic/controller/DeleteController.java
@@ -0,0 +1,16 @@
+package com.boot.basic.controller;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/delete-api")
+public class DeleteController {
+
+    @DeleteMapping("/delete/{variable}")
+    public String DeleteVariable(@PathVariable String variable){
+        return variable;
+    }
+}

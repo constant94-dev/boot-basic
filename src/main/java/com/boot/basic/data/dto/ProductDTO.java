cd366ecf98ecc96da457c48ceccabd2a4da105a9
Aaron(constant94-dev) <constant94@naver.com>
2024-01-29 20:27:14 +0900

    feat(dto): productDTO validation 어노테이션 추가

--- src/main/java/com/boot/basic/data/dto/ProductDTO.java
+++ src/main/java/com/boot/basic/data/dto/ProductDTO.java
@@ -1,6 +1,9 @@
 package com.boot.basic.data.dto;
 
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,8 +16,19 @@ import lombok.ToString;
 @ToString
 @Builder
 public class ProductDTO {
+    @NotNull
     private String productId;
+
+    @NotNull
     private String productName;
+
+    @NotNull
+    @Min(value = 500)
+    @Max(value = 3000000)
     private int productPrice;
+
+    @NotNull
+    @Min(value = 0)
+    @Max(value = 9999)
     private int productStock;
 }
